fe0e0bf39095bca0c34249d6466d3b4801936aef
julio <juliofariadev@gmail.com>
2024-12-06 23:42:18 -0300

    feat(components): create sign up form

--- frontend/src/app/Auth/components/FormSignup.tsx
+++ frontend/src/app/Auth/components/FormSignup.tsx
@@ -1,29 +1,46 @@
-'use client';
+"use client";
 
 import React from "react";
+import ButtonToggle from "./ButtonToggle";
+import GoogleSignup from "@/src/app/auth/components/GoogleSignup";
+import DividerWithText from "@/src/app/auth/components/DividerWithText";
+import InputWithLabel from "@/src/app/auth/components/InputWithLabel";
+import Button from "@/src/app/design-system/Button";
 
 enum SignupUserType {
-    jobSeeker,
-    company
+  jobSeeker,
+  company,
 }
 
 export default function FormSignup() {
-    console.log(0)
+  const [jobSeekerOrCompany, setJobSeekerOrCompany] =
+    React.useState<SignupUserType>(SignupUserType.jobSeeker);
 
-    const [jobSeekerOrCompany, setJobSeekerOrCompany] = React.useState<SignupUserType>(SignupUserType.jobSeeker);
+  const toggleJobSeekerOrCompany = (userType: SignupUserType) => {
+    setJobSeekerOrCompany(userType);
+  };
 
-    const toggleJobSeekerOrCompany = (userType: SignupUserType) => {
-        setJobSeekerOrCompany(userType)
-    }
-
-    return (
+  return (
+      <form>
         <div>
-            <button onClick={() => toggleJobSeekerOrCompany(SignupUserType.jobSeeker)}>
-                <p className={`text-indigo-700  font-semibold p-3 ${jobSeekerOrCompany === SignupUserType.jobSeeker && "bg-indigo-200"}`}>Job Seeker</p>
-            </button>
-            <button onClick={() => toggleJobSeekerOrCompany(SignupUserType.company)}>
-                <p className={`text-indigo-700  font-semibold p-3 ${jobSeekerOrCompany === SignupUserType.company && "bg-indigo-200"}`}>Company</p>
-            </button>
+          <ButtonToggle
+              isActive={jobSeekerOrCompany === SignupUserType.jobSeeker}
+              label="Job Seeker"
+              onClick={() => toggleJobSeekerOrCompany(SignupUserType.jobSeeker)}
+          />
+          <ButtonToggle
+              isActive={jobSeekerOrCompany === SignupUserType.company}
+              label="Company"
+              onClick={() => toggleJobSeekerOrCompany(SignupUserType.company)}
+          />
         </div>
-    )
+        <h2 className={"custom-dark text-3xl"}>Get more opportunities</h2>
+        <GoogleSignup/>
+        <DividerWithText label={"Or sign up with email"} />
+          <InputWithLabel labelText={"Full Name"} InputPlaceHolder={"Enter you full name"} />
+          <InputWithLabel labelText={"Email Address"} InputPlaceHolder={"Enter email address"} />
+          <InputWithLabel labelText={"Password"} InputPlaceHolder={"Enter password"} />
+          <Button text={"Continue"} textColor={"text-white"} backgroundColor={"bg-indigo-600"} />
+      </form>
+  );
 }
